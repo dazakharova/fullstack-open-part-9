c8641a375eae76aee2c45505a4c60cc270d63c26
Daria Zakharova <daria.zakharova1404@gmail.com>
2024-12-17 01:00:15 +0200

    Setup Patientor backend and implement /ping endpoint (ex.9.8-9.9)

--- patientor/index.ts
+++ patientor/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 const app = express();
+import cors from 'cors';
 app.use(express.json());
+app.use(cors())
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
