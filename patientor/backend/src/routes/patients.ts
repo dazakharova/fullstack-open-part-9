0e2c5fe6209a3c68b391266deea055cdcc39d42f
Daria Zakharova <daria.zakharova1404@gmail.com>
2024-12-19 20:13:04 +0200

    Implement Zod validation for POST /api/patients

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,27 +1,46 @@
 import express from 'express';
+import { z } from 'zod';
 import patientsService from '../services/patientsService';
-import { Response } from 'express';
-import { NonSensitivePatientEntry } from '../types';
-import toNewPatientEntry from '../utils';
+import { Request, Response, NextFunction } from 'express';
+import {NewPatientEntry, NonSensitivePatientEntry, Patient} from '../types';
+import { newPatientEntrySchema } from '../utils';
 
 const router = express.Router();
+
+const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
+    try {
+        newPatientEntrySchema.parse(req.body);
+        next();
+    } catch (error: unknown) {
+        next(error);
+    }
+};
+
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof z.ZodError) {
+        res.status(400).send({ error: error.issues });
+    } else {
+        next(error);
+    }
+};
 
 router.get('/', (_req, res: Response<NonSensitivePatientEntry[]>) => {
     res.send(patientsService.getNonSensitiveEntries());
 });
 
-router.post('/', (req, res) => {
+router.post('/', newPatientParser, (req: Request<unknown, unknown, NewPatientEntry>, res: Response<Patient | unknown>) => {
     try {
-        const newPatientEntry = toNewPatientEntry(req.body);
-        const addedEntry = patientsService.addPatientEntry(newPatientEntry);
+        const addedEntry = patientsService.addPatientEntry(req.body);
         res.json(addedEntry);
     } catch (error: unknown) {
-        let errorMessage = 'Something went wrong.';
-        if (error instanceof Error) {
-            errorMessage += ' Error: ' + error.message;
+        if (error instanceof z.ZodError) {
+            res.status(400).send({ error: error.issues });
+        } else {
+            res.status(400).send({ error: 'unknown error' });
         }
-        res.status(400).send(errorMessage);
     }
 });
+
+router.use(errorMiddleware);
 
 export default router;
--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -1,3 +1,6 @@
+import { z } from 'zod';
+import { newPatientEntrySchema } from './utils';
+
 export interface Diagnosis {
     code: string;
     name: string;
@@ -21,4 +24,4 @@ export enum Gender {
 
 export type NonSensitivePatientEntry = Omit<Patient, 'ssn'>;
 
-export type NewPatientEntry = Omit<Patient, 'id'>;
+export type NewPatientEntry = z.infer<typeof newPatientEntrySchema>;
