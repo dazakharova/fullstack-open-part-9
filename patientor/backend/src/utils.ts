c45200ff89b1122e59400cd97ca9bc05e42d6768
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-01-03 17:00:38 +0200

    Update entry types (ex. 9.23)

--- patientor/backend/src/utils.ts
+++ patientor/backend/src/utils.ts
@@ -1,5 +1,37 @@
-import { NewPatientEntry, Gender } from './types';
+import { NewPatientEntry, Gender, HealthCheckRating } from './types';
 import { z } from 'zod';
+
+const BaseEntrySchema = z.object({
+    id: z.string(),
+    description: z.string(),
+    date: z.string().date(),
+    specialist: z.string(),
+    diagnosisCodes: z.array(z.string()).optional(),
+});
+
+const HealthCheckEntrySchema = BaseEntrySchema.extend({
+    type: z.literal("HealthCheck"),
+    healthCheckRating: z.nativeEnum(HealthCheckRating),
+});
+
+const HospitalEntrySchema = BaseEntrySchema.extend({
+    type: z.literal("Hospital"),
+    discharge: z.object({
+        date: z.string().date(),
+        criteria: z.string(),
+    }),
+});
+
+const OccupationalHealthcareEntrySchema = BaseEntrySchema.extend({
+    type: z.literal("OccupationalHealthcare"),
+    employerName: z.string(),
+    sickLeave: z.object({
+        startDate: z.string().date(),
+        endDate: z.string().date(),
+    }).optional(),
+});
+
+const EntrySchema = z.union([HealthCheckEntrySchema, HospitalEntrySchema, OccupationalHealthcareEntrySchema]);
 
 export const newPatientEntrySchema = z.object({
     name: z.string(),
@@ -7,7 +39,7 @@ export const newPatientEntrySchema = z.object({
     ssn: z.string(),
     gender: z.nativeEnum(Gender),
     occupation: z.string(),
-    entries: z.array(z.unknown()).default([]),
+    entries: z.array(EntrySchema).default([]),
 });
 
 const toNewPatientEntry = (object: unknown): NewPatientEntry => {
