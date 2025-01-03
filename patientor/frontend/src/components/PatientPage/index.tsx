b38646b27590dd6891756e1486f4ae7f077d6cf0
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-01-03 13:05:39 +0200

    Implement patient detail page with React Router and data fetching (ex. 9.22)

---
+++ patientor/frontend/src/components/PatientPage/index.tsx
@@ -0,0 +1,40 @@
+import { useParams } from "react-router-dom";
+import { Patient } from "../../types.ts";
+import patientService from "../../services/patients.ts";
+import FemaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
+import {useEffect, useState} from "react";
+
+const PatientPage = () => {
+    const [patient, setPatient] = useState<Patient>();
+    const id  = useParams().id;
+    if (!id) return null;
+
+    useEffect(() => {
+        const fetchPatient = async () => {
+            const patient = await patientService.getById(id);
+            setPatient(patient);
+        }
+        void fetchPatient();
+    }, []);
+
+
+    if (!patient) {
+        return <div>Patient not found!</div>;
+    }
+
+    return (
+        <div>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+                <h2 style={{margin: 0, paddingRight: '10px'}}>{patient.name}</h2>
+                <span>
+                    {patient.gender === 'male' ? <MaleIcon/> : (patient.gender === 'female' ? <FemaleIcon/> : null)}
+                </span>
+            </div>
+            {patient.ssn ? (<p>ssn: {patient.ssn}</p>) : null}
+            <p>occupation: {patient.occupation}</p>
+        </div>
+    )
+}
+
+export default PatientPage;
